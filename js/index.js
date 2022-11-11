349900f08a52b791fcf29de2f69bdc81c25c5c57
Irina Safarova <irinasfv19@gmail.com>
2022-11-11 21:12:51 +0300

    Итерация2

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
+//Переменные
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button'); 
-
-
-editButton.addEventListener('click', function (popupOpen) {
-    popup.classList.add('popup_opened')
-  })
-
-closeButton.addEventListener('click', function (popupClose) {
-    popup.classList.remove('popup_opened')
-  })
-
-
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__user-info');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_user-info');
+
+// Функции
+function popupOpen() {
+  popup.classList.add('popup_opened')
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened')
+}
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    let newNameInput = document.querySelector('.profile__title');
-    let newJobInput = document.querySelector('.profile__description');
-    newNameInput.textContent = nameInput.value;
-    newJobInput.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+  evt.preventDefault();
+  let newNameInput = document.querySelector('.profile__title');
+  let newJobInput = document.querySelector('.profile__description');
+  newNameInput.textContent = nameInput.value;
+  newJobInput.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
 
 }
+
+//События
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler); 
